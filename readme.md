341007cd1afb0dd74c449905bf9b510b217dd49b
Ransu-ll <lancekyao@gmail.com>
2022-01-28 14:30:51 +1100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,69 @@
+# Saved Scraper - Scraping your saved posts on Reddit
+
+
+## Introduction
+A script designed to go through your saved posts and comments on Reddit, put them in an SQLite database and 
+optionally export it to a `.csv` file for ease of access. 
+
+If the saved content already exists in the database, then it will not be duplicated.
+
+It can be also used as a module if desired.
+
+Note: If you have more than 1000 comments and posts saved, this script will not be able to capture all of them due to
+limitations in the API. See [Obfuscation and API limitations](https://praw.readthedocs.io/en/v3.6.2/pages/getting_started.html#obfuscation-and-api-limitations).
+
+## Installation
+
+### Part 1 - Creating a Reddit application
+1. Go to your Reddit account's [authorised applications](https://www.reddit.com/prefs/apps/) page.
+2. Find the button that allows you to create an app and click on it.
+3. The `name` can be set to anything, I recommend "saved-scraper". The `description` and `about` can be left blank, 
+however, the `uri` must be set to something. I recommend "http://www.example.com/unused/redirect/uri" as per the 
+outdated but useful nonetheless 
+[archived PRAW (Python Reddit API Wrapper) documentation](https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example).
+It will not be all that relevant for this script anyway.
+4. Press the "create app" button. Note the `client id`, located below the text that says "personal use script", and 
+`client secret` as they will be used in the next section.
+
+***WARNING: KEEP `client id` AND `client secret` PRIVATE - THIS SHOULD ONLY BE KNOWN BY YOU.***
+
+### Part 2 - Installing the script
+Note: You will need to have installed Python 3.10+ for this script to work. Compatibility with older versions not
+guaranteed.
+
+Note: You will need to install 3rd party modules `praw` and `dotenv`. If unsure how to install them, see  the offical
+document [Installing Python Modules](https://docs.python.org/3/installing/index.html). The former is used to access your
+Reddit account, the latter is for isolating your login details, as well as `client id` and `client secret`.
+
+1. Clone or download this repository.
+2. [Pick branch of choice, see subheadings]
+
+    #### 2a. Using a `.env` file (recommended)
+    In the same folder which [the script](scrapesaved.py) is in, create a file called `.env`.
+    
+    The .env should look like the following:
+    ```
+    CLIENT_ID=[INSERT "client id"]
+    CLIENT_SECRET=[INSERT "client secret"]
+    USER_AGENT=desktop:saved_scraper:v0.0.1 (by u/[INSERT YOUR USERNAME])
+    
+    USER_NAME=[INSERT YOUR USERNAME]
+    USER_PASS=[INSERT YOUR PASSWORD]
+    ```
+    Where there are brackets, do not include them when you insert values. For example, `CLIENT_ID=AbCDEF_G192V`.  
+    `CLIENT_ID` is 22 characters long, `CLIENT_SECRET` is 30 characters long.
+    
+    #### 2b. Manually inputting the details
+    In [the script](scrapesaved.py), where `main()` is called set `True` to `False` and it will prompt you to add the
+    details via the `input()` function.
+
+
+3. Run the file! By default, it will scrape all of your saved content, create a database and put the data there and then
+export the contents of the database to a `.csv` file.
+
+## Using the produced `.csv` file
+- `Sub` - the Subreddit which the post/comment belongs to
+- `Type` - if it is a comment or a post. `0` means post, `1` means comment
+- `ID` - The ID of the content.
+- `Title` - The title of the post. If it is a comment, it is the title of the post which it is under.
+- `URL` - The link to it. Put `reddit.com` before the URL listed to access the link directly to it.
---
+++ scrapesaved.py
@@ -0,0 +1,135 @@
+import praw
+from prawcore import OAuthException
+from dotenv import load_dotenv, dotenv_values
+import sqlite3
+import csv
+
+
+def get_user_info(_id: str, secret: str, agent: str, 
+                  user: str, _pass: str, _2fa: str) -> praw.Reddit:
+    """Get access to a Reddit account"""
+    reddit_user = praw.Reddit(client_id=_id,
+                              client_secret=secret,
+                              user_agent=agent,
+                              username=user,
+                              password=_pass + _2fa)
+    reddit_user.read_only = True
+
+    # Check that the credentials are valid.
+    try:
+        print("User:", reddit_user.user.me())
+    except OAuthException as e:
+        raise Exception(e, "TL;DR: Invalid credential(s)")
+    return reddit_user
+
+
+def access_database(name: str) -> tuple[sqlite3.Cursor, sqlite3.Connection]:
+    """Access the database containing saved content"""
+    
+    con = sqlite3.connect(name)
+    cur = con.cursor()
+
+    # Check if table exists, if not create it.
+    # Note: If type = 0, it is a post.
+    # Note: If typ6e = 1, it is a comment
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS "saved" (
+                    Sub TEXT NOT NULL,
+                    Type  TEXT NOT NULL,
+                    ID    TEXT NOT NULL,
+                    Title    TEXT NOT NULL,
+                    URL   TEXT NOT NULL,
+                    PRIMARY KEY("ID")
+                );''')
+    return cur, con
+
+
+def export_database(name_database: str, name_export: str = "exported.csv") -> None:
+    """Export the database table 'saved' to csv"""
+    
+    database = access_database(name_database)
+    cur = database[0]
+    con = database[1]
+
+    headers = cur.execute("SELECT name FROM PRAGMA_TABLE_INFO('saved')")
+    header_names = [i[0] for i in headers]
+
+    with open(name_export, mode="w", encoding='utf-16') as f:
+        # utf-16 is used to support more characters since Reddit
+        # allows for a variety of characters for titles
+        writer = csv.writer(f, lineterminator="\n", delimiter="\t")
+        # Delimiter changed to tabs so that Excel can properly
+        # display the columns
+        writer.writerow(header_names)
+        writer.writerows(cur.execute("SELECT * FROM saved"))
+    con.close()
+
+
+def main(name: str, use_file: bool = True) -> None:
+    """The main program.
+
+    :param name: The name of the database to view
+    :param use_file: Whether or not to use a .env file for details
+    """
+    # Load credentials from .env file
+    load_dotenv()
+    config = dotenv_values(".env")
+
+    # Access database and variables
+    database = access_database(name)
+    cur = database[0]
+    con = database[1]
+
+    # Access Reddit account
+    if use_file:
+        _2FA = ":" + input("2FA (if none, press enter): ")
+        reddit = get_user_info(config["CLIENT_ID"], config["CLIENT_SECRET"],
+                               config["USER_AGENT"], config["USER_NAME"],
+                               config["USER_PASS"], _2FA)
+    else:
+        _client_id = input("CLIENT_ID: ")
+        _client_secret = input("CLIENT_SECRET: ")
+        _user_agent = input("USER_AGENT: ")
+        _user_name = input("USER_NAME: ")
+        _user_pass = input("USER_PASS: ")
+        _2FA = input("2FA (if applicable, if not leave blank and press enter): ")
+
+        reddit = get_user_info(_client_id, _client_secret, _user_agent,
+                               _user_name, _user_pass, _2FA)
+    # Record the saved posts and comments
+    _note = ""
+    for index, item in enumerate(reddit.user.me().saved(limit=None)):
+        if isinstance(item, praw.reddit.models.Submission):
+            content = {"sub": item.subreddit.display_name,
+                       "type": 0,
+                       "id": item.id,
+                       "title": item.title,
+                       "url": item.permalink}
+
+        elif isinstance(item, praw.reddit.models.Comment):
+            content = {"sub": item.subreddit.display_name,
+                       "type": 1,
+                       "id": item.id,
+                       "title": item.submission.title,
+                       "url": item.permalink}
+        else:
+            raise Exception("What was retrieved is neither a post nor comment. Suspicious.")
+
+        try:
+            # TODO: Use named style in the future
+            cur.execute('''INSERT INTO saved VALUES
+                        (?, ?, ?, ?, ?)''',
+                        list(content.values()))
+            con.commit()
+            _note = "Success"
+        except sqlite3.IntegrityError:
+            _note = "Already in database"
+        
+        print(index, f"id: {content['id']}", f"type: {content['type']}", _note, sep=" - ")
+    con.close()
+
+
+if __name__ == "__main__":
+    database_name = "information.db"
+    main(database_name, True)
+    export_database(database_name)
